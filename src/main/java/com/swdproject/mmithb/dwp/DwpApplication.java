519d33d5c34972d3ce45f16bd8b6d369f164971d
danyhp <dany.panggabean@gmail.com>
2018-05-22 16:28:29 +0700

    - COLUMN_ACTION DATATABLE IMPLEMENTATION(DANY)

--- src/main/java/com/swdproject/mmithb/dwp/DwpApplication.java
+++ src/main/java/com/swdproject/mmithb/dwp/DwpApplication.java
@@ -2,13 +2,10 @@ package com.swdproject.mmithb.dwp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.datatables.repository.DataTablesRepositoryFactoryBean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaAuditing
-@EnableJpaRepositories(repositoryFactoryBeanClass = DataTablesRepositoryFactoryBean.class)
 public class DwpApplication {
 
     public static void main(String[] args) {
